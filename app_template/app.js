3acbd91ca871c0406e4036b8ba009cf75f7dd08e
zong-ying.cao <cao5zy@163.com>
2018-05-08 21:41:00 +0800

    fix a bug in log4js configuration

--- app_template/app.js
+++ app_template/app.js
@@ -1,10 +1,11 @@
 (function(factory){
     module.exports = factory(require('seneca')(),
-			     require('log4js').getLogger('app'),
+			     require('log4js'),
 			     require('./service'),
 			     require('./config')
 			    );
-}(function(seneca, logger, service, config){
+}(function(seneca, log4js, service, config){
+    log4js.configure("./config/log4js.json");
     seneca.use(service);
     seneca.listen(config.port);
 }));

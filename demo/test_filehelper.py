cccda2540c5e1be25758833da98c4e9cacc5b09f
alan.cao <zongying.cao@hpe.com>
2018-07-05 21:54:52 -0400

    refactor with name
    Signed-off-by: alan.cao <zongying.cao@hpe.com>

---
+++ demo/test_filehelper.py
@@ -0,0 +1,25 @@
+from nose import with_setup
+from assertpy import assert_that
+import os
+from util.filehelper import collect_files
+from util.env import put_folder, put_file
+from util.debug import on
+
+
+root = "./.test"
+
+def setup_test_collect_file():
+    put_file("file2.ce", put_folder("folder1", put_folder(root)), "")
+    put_file("file2.xs", put_folder("folder2", put_folder(root)), "")
+    put_file("file1.ce", put_folder(root), "")
+    put_file("file2.xd", put_folder(root), "")
+
+def clear():
+    pass
+
+@with_setup(setup_test_collect_file, clear)
+def test_collect_file():
+    complete_path = os.path.abspath(root)
+    assert_that(collect_files(root, "ce")) \
+        .contains(complete_path + "/file1.ce", complete_path + "/folder1/file2.ce") \
+        .does_not_contain(complete_path + "/file2.xd", complete_path + "/folder2/file2.xs")

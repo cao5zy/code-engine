eddb5f59c2b2947718c91254f738d3246a7b3ccd
Ubuntu <caozon@code-dev.4co5vkhwnyru5da0oj2ltjzlib.hx.internal.cloudapp.net>
2018-07-12 14:04:39 +0000

    add init files
    Signed-off-by: Ubuntu <caozon@code-dev.4co5vkhwnyru5da0oj2ltjzlib.hx.internal.cloudapp.net>

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+requires = ['demjson'],
+
+setup(
+    name = 'code_engine',
+    version = '1.2.1',
+    author = 'Zongying Cao',
+    author_email = 'zongying.cao@dxc.com',
+    description = 'code engine',
+    long_description = 'code engine',
+    url = 'https://github.com/cao5zy/code-engine',
+    packages = packages,
+    install_requires = requires,
+    license = 'Apache',
+    classifiers = [
+               'Development Status :: 4 - Beta',
+               'Intended Audience :: Developers',
+               'License :: OSI Approved :: Apache Software License',
+               'Natural Language :: English',
+               'Operating System :: OS Independent',
+               'Programming Language :: Python :: 3',
+               'Topic :: Software Development :: Libraries',
+           ],
+)

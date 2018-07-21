d12a6a5310b77e1213f8994a4bd8a117ad7c92de
Ubuntu <caozon@code-dev.4co5vkhwnyru5da0oj2ltjzlib.hx.internal.cloudapp.net>
2018-07-21 03:22:54 +0000

    refactor the name
    Signed-off-by: Ubuntu <caozon@code-dev.4co5vkhwnyru5da0oj2ltjzlib.hx.internal.cloudapp.net>

--- code_engine/app.py
+++ code_engine/app.py
@@ -3,16 +3,16 @@ from .code_engine_file import get_value
 from .code_engine_file import push_values
 from .code_engine_core import gen
 
-def publish(folderpath, subscribe_name, data):
+def publish(folderpath, subscribe_name, data, output_path = None):
 
-    def generate(template_path):
-        gen(get_value(template_path, "template_path"), \
-            get_value(template_path, "variables"), \
-            get_value(template_path, "output_path") \
+    def generate(definition_file_path):
+        gen(get_value(definition_file_path, "template_path"), \
+            get_value(definition_file_path, "variables"), \
+            get_value(definition_file_path, "output_path") \
             )
         
-    def handle(files):
-        list([generate(push_values(file, data)) for file in files if get_value(file, "subscribe_name") == subscribe_name])
+    def handle(definition_files):
+        list([generate(push_values(file, data)) for file in definition_files if get_value(file, "subscribe_name") == subscribe_name])
 
     handle(collect_files(folderpath, "ce"))
 
